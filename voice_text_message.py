631e1722404ba3a1e55e97c9e706063d41b7ae17
Koushal Bhat <60033544+koushal2001@users.noreply.github.com>
2021-02-21 21:59:55 +0530

    Add files via upload
    With an added feature now you can text your fiends through voice

---
+++ voice_text_message.py
@@ -0,0 +1,34 @@
+import speech_recognition as speech
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+listener = speech.Recognizer()
+
+driver=webdriver.Chrome("/home/koushal/selenium/chromedriver")   # Selenium chromedriver path
+
+driver.get("https://web.whatsapp.com/")
+
+# name=input("Say the Target Name after scanning QR code.\n ")
+
+# with speech.Microphone() as source:
+#     print("NOw")
+#     message=listener.listen(source)
+#     message=listener.recognize_google(message)
+#     print(message)
+input()
+
+name="Mummy"
+
+
+user = driver.find_element_by_xpath("//span[@title='{}']".format(name))
+user.click()
+
+text_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+with speech.Microphone() as source:
+    print("NOw2")
+    message=listener.listen(source)
+    message=listener.recognize_google(message)
+    print(message)
+for i in range(10):
+    text_box.send_keys(message+Keys.ENTER)
+driver.quit()
